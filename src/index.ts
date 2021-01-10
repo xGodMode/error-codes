a8de5da3a5b3f1428651c8a0e916d73e00ea3747
valmack <8445610+valmack@users.noreply.github.com>
2021-01-10 18:03:40 -0500

    feat: add metadata to errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 class CustomError extends Error {
   public readonly code: string
   public readonly timestamp: number
+  public readonly metadata: {[key: string]: any}
 
-  constructor(name: string, code: string, ...params: any) {
+  constructor(name: string, code: string, metadata?: any, ...params: any) {
     super(...params)
 
     // Maintains proper stack trace for where our error was thrown (only available on V8)
@@ -13,10 +14,11 @@ class CustomError extends Error {
     this.name = name
     this.code = code
     this.timestamp = Date.now()
+    this.metadata = metadata
   }
 }
 
-export const HTTPError = (statusCode?: number): CustomError => {
+export const HTTPError = (statusCode?: number, message?: string): CustomError => {
   const code = statusCode ? `HTTP${statusCode}` : `HTTP0`
-  return new CustomError('HTTPError', code)
+  return new CustomError('HTTPError', code, {statusCode}, message)
 }
