773c34a414e7b688722b6898f6ffcb15837edb5e
valmack <8445610+valmack@users.noreply.github.com>
2021-01-24 00:06:38 -0500

    feat: add GM, CAIP, and Protocol errors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,19 @@ class CustomError extends Error {
   }
 }
 
+export const GMError = (options: {subCode?: string, message?: string, baseError?: Error}): CustomError => {
+  return new CustomError('GMError', `GM${options.subCode || ''}`, {baseError: options.baseError}, options.message)
+}
+
 export const HTTPError = (statusCode?: number, message?: string): CustomError => {
   const code = statusCode ? `HTTP${statusCode}` : `HTTP0`
   return new CustomError('HTTPError', code, {statusCode}, message)
+}
+
+export const CAIPNetworkError = (options: {subCode?: string, message?: string}): CustomError => {
+  return new CustomError('CAIPNetworkError', `CAIP_N${options.subCode || ''}`, null, options.message)
+}
+
+export const ProtocolError = (options: {subCode?: string, message?: string}): CustomError => {
+  return new CustomError('ProtocolError', `PROTO${options.subCode || ''}`, null, options.message)
 }
